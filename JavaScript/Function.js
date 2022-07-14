736d1d2195c2b321e827da6a74c3315edd8b6229
Eufrásio junior <juniorrochaa@gmail.com>
2022-07-13 21:05:11 -0300

    updates

---
+++ JavaScript/Function.js
@@ -0,0 +1,21 @@
+function calcularMedia(){
+    let total = 0
+    let qtd = arguments.length
+    let x = 0
+
+    while(arguments[x] === 'number'){
+        total += arguments[x]
+        x++
+    }
+
+    return (total / qtd).toFixed(2)
+}
+
+function sortear(n){
+    let _n = n || 1
+    let nSorteado = Math.random()
+    nSorteado = nSorteado * _n
+    nSorteado = Math.floor(nSorteado)
+
+    return nSorteado
+}
